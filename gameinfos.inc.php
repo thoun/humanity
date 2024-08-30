952772a03b773f9c124524bf551bce95a183dff1
Guy <guy@boardgamearena.com>
2024-08-30 14:44:45 +0200

    bga-zoom 1.0.4

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -15,15 +15,6 @@ $gameinfos = [
 // Name of the game in English (will serve as the basis for translation) 
 'game_name' => "Humanity",
 
-// Game designer (or game designers, separated by commas)
-'designer' => 'Yoann Levet',       
-
-// Game artist (or game artists, separated by commas)
-'artist' => 'Paul Chadeisson, Pierre Lazarevic, Remy Paul',         
-
-// Year of FIRST publication of this game. Can be negative.
-'year' => 2023,
-
 // Game publisher (use empty string if there is no publisher)
 'publisher' => 'Bombyx',
 
@@ -88,18 +79,6 @@ $gameinfos = [
 // NB: the default will be the first language in this list spoken by the player, so you should list them by popularity/preference.
 'language_dependency' => false,
 
-// Complexity of the game, from 0 (extremely simple) to 5 (extremely complex)
-'complexity' => 4,  
-
-// Strategy of the game, from 0 (no strategy can be setup) to 5 (totally based on strategy)
-'strategy' => 4,      
-
-// Luck of the game, from 0 (absolutely no luck in this game) to 5 (totally luck driven)
-'luck' => 2,    
-
-// Diplomacy of the game, from 0 (no interaction in this game) to 5 (totally based on interaction and discussion between players)
-'diplomacy' => 3,    
-
 // Colors attributed to players
   'player_colors' => [
     'f0db6c', // yellow
@@ -119,38 +98,12 @@ $gameinfos = [
 // Game interface width range (pixels)
 // Note: game interface = space on the left side, without the column on the right
 'game_interface_width' => [
+    'autoscale' => true,
 
     // Minimum width
     //  default: 740
     //  maximum possible value: 740 (ie: your game interface should fit with a 740px width (correspond to a 1024px screen)
     //  minimum possible value: 320 (the lowest value you specify, the better the display is on mobile)
     'min' => 1181,
-
-    // Maximum width
-    //  default: null (ie: no limit, the game interface is as big as the player's screen allows it).
-    //  maximum possible value: unlimited
-    //  minimum possible value: 740
-    'max' => null
   ],
-
-// Game presentation
-// Short game presentation text that will appear on the game description page, structured as an array of paragraphs.
-// Each paragraph must be wrapped with totranslate() for translation and should not contain html (plain text without formatting).
-// A good length for this text is between 100 and 150 words (about 6 to 9 lines on a standard display)
-'presentation' => [
-    totranslate("Humanity is a game of anticipation, very close to our future (in 2097)."),
-    totranslate("In this tile and worker placement game for 2 to 4 players, they will have to manage resources and their time to build a base to be used by scientific civilian settlers. Players will have to assemble the modules produced by the automated station too and carry out experiments to further research."),
-    totranslate("In their turn, players can choose to produce resources or spend them by sending one of their 3 astronauts to the automated station to get new base modules or carry out experiments to further research."),
-    totranslate("But time could not be the same for all: the further you go to get a specific module, the later you will get back to your scientific base to connect it..."),
-],
-
-//////// BGA SANDBOX ONLY PARAMETERS (DO NOT MODIFY)
-
-// simple : A plays, B plays, C plays, A plays, B plays, ...
-// circuit : A plays and choose the next player C, C plays and choose the next player D, ...
-// complex : A+B+C plays and says that the next player is A+B
-'is_sandbox' => false,
-'turnControl' => 'simple'
-
-////////
 ];
